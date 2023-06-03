f3f632b52ddbd7ccfa2dfacd0d43ebab3aefdb3a
Fernanda <107973615+fernandaib@users.noreply.github.com>
2023-06-02 21:44:24 -0700

    23-06-02
    **This Folder get Van's upgrades
    
    Changes committed:
    1-Change of [index.css] by [for-schools-styling.css]
    
    *Over [for-schools-styling.css]
    2-Font-size was added lines 28 to 34
    3-Font size was changed since the data, set before, indicated font weight and not font size line 48
    4-Code to style typography with Font-Size created (look at #2 item of this list) lines 196 to 234
    
    *Over [styling.html]
    
    1-Set plain text code over Reset section
    2-Upgrade plain text code over Typography section
    3-Set plain text code over Grid section (it is possible this change due to the grid is not responsive)

---
+++ group04Project_v04/scripts/script.js
@@ -0,0 +1,6 @@
+function myFunction() {    
+    document.getElementById("aside-menu").classList.toggle("display-aside-menu");
+    document.getElementById("open").classList.toggle("switchOff");
+    document.getElementById("close").classList.toggle("switchOn");
+    
+  }
